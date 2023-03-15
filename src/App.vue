735c14c8ba97d9c6995829c2e641b6450b6fee0a
VictorGiuliano <victorgiuliano98@gmail.com>
2023-03-15 21:49:19 +0100

    Milestone 5 complete

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script>
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+import AppCard from './components/AppCard.vue';
 const apiBaseUrl = 'http://127.0.0.1:8000/api/';
 export default {
   name: 'App',
-  components: { AppHeader },
+  components: { AppHeader, AppCard },
   data: () => ({
     projects: []
   }),
   methods: {
     fetchProjects() {
       axios.get(apiBaseUrl + 'projects').then(res => {
-        console.log(res.data);
+        this.projects = res.data;
       });
     }
   },
@@ -23,5 +24,11 @@ export default {
 </script>
 <template>
   <AppHeader />
+  <div class="container">
+    <div v-if="projects.length">
+      <AppCard :project="project" v-for="project in projects" :key="project.id" />
+    </div>
+    <h3 v-else class="text-center">Non ci sono progetti</h3>
+  </div>
 </template>
 <style></style>
---
+++ src/components/AppCard.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'AppCard',
+    props: { project: Object }
+};
+</script>
+<template>
+    <div class="card mt-5">
+        <div class="card-body">
+            <h5 class="card-title">{{ project.title }}</h5>
+            <p class="card-text">{{ project.description }}</p>
+        </div>
+    </div>
+</template>
