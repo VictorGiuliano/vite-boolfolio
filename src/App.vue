bb1a90d9a16096cf95baeca14984ed2a55884d0e
VictorGiuliano <victorgiuliano98@gmail.com>
2023-03-15 21:21:48 +0100

    Milestone 4 complete

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,24 @@
 <script>
+import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/';
 export default {
   name: 'App',
-  components: { AppHeader }
-
-}
+  components: { AppHeader },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + 'projects').then(res => {
+        console.log(res.data);
+      });
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+};
 
 </script>
 <template>
