1c50774e68a91db757d4a8966bd5fa4f737f41f8
VictorGiuliano <victorgiuliano98@gmail.com>
2023-03-15 18:53:28 +0100

    Complete Milestone 3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
   name: 'App',
-};
+  components: { AppHeader }
+
+}
+
 </script>
-<template></template>
+<template>
+  <AppHeader />
+</template>
 <style></style>
