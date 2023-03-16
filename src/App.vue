2a633d0f518bad361cc503a306cf4713b019a5f1
VictorGiuliano <victorgiuliano98@gmail.com>
2023-03-16 17:35:58 +0100

    Installed Vue router

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,14 @@
 <script>
-import axios from 'axios';
+import HomePage from './pages/HomePage.vue';
 import AppHeader from './components/AppHeader.vue';
-import AppCard from './components/AppCard.vue';
-const apiBaseUrl = 'http://127.0.0.1:8000/api/';
 export default {
   name: 'App',
-  components: { AppHeader, AppCard },
-  data: () => ({
-    projects: []
-  }),
-  methods: {
-    fetchProjects() {
-      axios.get(apiBaseUrl + 'projects').then(res => {
-        this.projects = res.data;
-      });
-    }
-  },
-  created() {
-    this.fetchProjects();
-  }
+  components: { AppHeader, HomePage },
 };
 
 </script>
 <template>
   <AppHeader />
-  <div class="container">
-    <div v-if="projects.length">
-      <AppCard :project="project" v-for="project in projects" :key="project.id" />
-    </div>
-    <h3 v-else class="text-center">Non ci sono progetti</h3>
-  </div>
+  <HomePage />
 </template>
 <style></style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import AppCard from '../components/AppCard.vue';
+const apiBaseUrl = 'http://127.0.0.1:8000/api/';
+export default {
+    name: 'Homepage',
+    components: { AppCard },
+    data: () => ({
+        projects: []
+    }),
+    methods: {
+        fetchProjects() {
+            axios.get(apiBaseUrl + 'projects').then(res => {
+                this.projects = res.data;
+            });
+        }
+    },
+    created() {
+        this.fetchProjects();
+    }
+};
+</script>
+<template>
+    <div class="container">
+        <div v-if="projects.length">
+            <AppCard :project="project" v-for="project in projects" :key="project.id" />
+        </div>
+        <h3 v-else class="text-center">Non ci sono progetti</h3>
+    </div>
+</template>
+<style></style>
